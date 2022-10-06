a3cd232f2ee8b5cee8b16c305a4f4debd6ade3e4
Taylor Heyen <taylordheyen@gmail.com>
2022-10-05 21:00:22 -0700

    Updating JS file

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ function PageTransitions() {
                 section.classList.remove('active')
             })
             const element = document.getElementById(id);
+            element.classList.add('active');
         }
     })
 }
